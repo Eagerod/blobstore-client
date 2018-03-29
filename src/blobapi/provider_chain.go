dc7ae3f6c120b709976043342f305c0537964f20
Aleem Haji <hajial@gmail.com>
2018-03-29 16:18:55 -0400

    Untested implementation of the environment credential provider.

--- src/blobapi/provider_chain.go
+++ src/blobapi/provider_chain.go
@@ -1,46 +1,83 @@
 package blobapi;
 
 import (
-    "http"
+    "net/http"
+    "os"
+)
+
+const (
+    HttpRequestReadAclHeader = "X-BlobStore-Read-Acl"
+    HttpRequestWriteAclHeader = "X-BlobStore-Write-Acl"
+
+    DefaultBlobStoreReadAclEnvironmentVariable = "BLOBSTORE_READ_ACL"
+    DefaultBlobStoreWriteAclEnvironmentVariable = "BLOBSTORE_WRITE_ACL"
 )
 
 type CredentialProviderChain struct {
-    providers []*ICredentialProvider
+    providers []ICredentialProvider
 }
+
+var defaultPc *CredentialProviderChain 
 
 type ICredentialProvider interface {
     AuthorizeRequest(*http.Request) error
 }
 
-var defaultPc *ProviderChain 
+func HasReadAclHeader(request *http.Request) bool {
+    _, ok := request.Header[HttpRequestReadAclHeader]
+    return ok
+}
+
+func HasWriteAclHeader(request *http.Request) bool {
+    _, ok := request.Header[HttpRequestWriteAclHeader]
+    return ok
+}
 
 func DefaultCredentialProviderChain() *CredentialProviderChain {
-    if defaultPc {
+    if defaultPc != nil {
         return defaultPc
     }
 
-    defaultPc = new(ProviderChain)
-    // defaultPc.providers = append(defaultPc.providers)
+    defaultPc = new(CredentialProviderChain)
+
+    defaultPc.providers = append(defaultPc.providers,
+        &EvironmentCredentialProvider{
+            DefaultBlobStoreReadAclEnvironmentVariable,
+            DefaultBlobStoreWriteAclEnvironmentVariable,
+        },
+    )
     return defaultPc
 }
 
-func (*cpc CredentialProviderChain) AuthorizeRequest(request *http.Request) error {
+func (cpc *CredentialProviderChain) AuthorizeRequest(request *http.Request) error {
     for _, provider := range cpc.providers {
-        if err := provider.AuthorizeRequest(request); err == nil {
+        if err := provider.AuthorizeRequest(request); err != nil {
+            return err
+        }
+        if HasReadAclHeader(request) && HasWriteAclHeader(request) {
             return nil
         }
     }
 
-    return errors.New("Failed to find any mechanism to authenticate requests to Blobstore")
+    // Maybe no authorization is desired.
+    return nil
 }
 
 type EvironmentCredentialProvider struct {
-    readEnvVar string
-    writeEnvVar string
+    ReadAclEnvironmentVariable string
+    WriteAclEnvironmentVariable string
 }
 
 func (ecp *EvironmentCredentialProvider) AuthorizeRequest(request *http.Request) error {
-    
-    request.Header.Add("X-BlobStore-Read-Acl", b.DefaultReadAcl)
-    request.Header.Add("X-BlobStore-Write-Acl", b.DefaultWriteAcl)
+    if !HasReadAclHeader(request) {
+        if acl, ok := os.LookupEnv(ecp.ReadAclEnvironmentVariable); ok {
+            request.Header.Add(HttpRequestReadAclHeader, acl)
+        }
+    }
+    if !HasWriteAclHeader(request) {
+        if acl, ok := os.LookupEnv(ecp.WriteAclEnvironmentVariable); ok {
+            request.Header.Add(HttpRequestWriteAclHeader, acl)
+        }
+    }
+    return nil
 }
