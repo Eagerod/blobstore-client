9f1fe6ce6cc12f2dd1711a4d389e1dae6cbb66f8
Aleem Haji <hajial@gmail.com>
2021-10-30 18:40:44 -0400

    Moves cp logic into client.

--- cmd/blobapi/cp_command.go
+++ cmd/blobapi/cp_command.go
@@ -1,11 +1,6 @@
 package blobapi
 
 import (
-	"errors"
-	"os"
-)
-
-import (
 	"github.com/spf13/cobra"
 )
 
@@ -30,46 +25,15 @@ func newCpCommand(client blob.IBlobStoreClient) *cobra.Command {
 			}
 
 			if len(args) == 1 {
-				if cpArg0.Scheme != BlobStoreUrlScheme {
-					return errors.New("Must download files from blob:/")
-				}
-				return client.CatFile(cpArg0.Path)
+				return client.Cat(cpArg0)
 			}
 
-			// Determine if this is an upload or download command based on which
-			// order the parameters came in.
 			cpArg1, err := newBlobParsedArg(args[1])
 			if err != nil {
 				return err
 			}
 
-			if cpArg0.Scheme == BlobStoreUrlScheme && cpArg1.Scheme == BlobStoreUrlScheme {
-				return errors.New("No support for copying files in the blobstore directly")
-			}
-
-			if cpArg0.Scheme != BlobStoreUrlScheme && cpArg1.Scheme != BlobStoreUrlScheme {
-				return errors.New("Must provide at least one blob:/ path to upload to or download from")
-			}
-
-			if cpArg0.Scheme == BlobStoreUrlScheme {
-				if force == false {
-					if _, err := os.Stat(cpArg1.Path); err == nil {
-						return errors.New("Destination file already exists on local machine; use --force to overwrite")
-					}
-				}
-				return client.DownloadFile(cpArg0.Path, cpArg1.Path)
-			} else {
-				if force == false {
-					fileStat, err := client.StatFile(cpArg1.Path)
-					if err != nil {
-						return err
-					}
-					if fileStat.Exists {
-						return errors.New("Destination file already exists on blobstore; use --force to overwrite")
-					}
-				}
-				return client.UploadFile(cpArg1.Path, cpArg0.Path, contentType)
-			}
+			return client.Copy(cpArg0, cpArg1, force)
 		},
 	}
 
--- pkg/blob/client.go
+++ pkg/blob/client.go
@@ -2,9 +2,11 @@ package blob
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -14,16 +16,20 @@ import (
 	"gitea.internal.aleemhaji.com/aleem/blobapi/pkg/credential_provider"
 )
 
+const BlobStoreUrlScheme string = "blob"
+
 type BlobStoreClient struct {
 	apiClient IBlobStoreApiClient
 }
 
 type IBlobStoreClient interface {
+	Cat(src *url.URL) error
+	Copy(src *url.URL, dst *url.URL, force bool) error
+
 	UploadFile(path string, source string, contentType string) error
 
 	GetFileContents(path string) (string, error)
 	DownloadFile(path string, dest string) error
-	CatFile(path string) error
 
 	StatFile(path string) (*BlobFileStat, error)
 
@@ -34,6 +40,8 @@ type IBlobStoreClient interface {
 	ListPrefix(prefix string, recursive bool) ([]string, error)
 
 	DeleteFile(path string) error
+
+	Exists(url_ url.URL) (bool, error)
 }
 
 func NewBlobStoreClient(url string, credentialProvider credential_provider.ICredentialProvider) *BlobStoreClient {
@@ -43,6 +51,43 @@ func NewBlobStoreClient(url string, credentialProvider credential_provider.ICred
 		apiClient,
 	}
 }
+
+func (b *BlobStoreClient) Copy(src *url.URL, dst *url.URL, force bool) error {
+	cpArg0 := src
+
+	// Determine if this is an upload or download command based on which
+	// order the parameters came in.
+	cpArg1 := dst
+
+	if cpArg0.Scheme == BlobStoreUrlScheme && cpArg1.Scheme == BlobStoreUrlScheme {
+		return errors.New("No support for copying files in the blobstore directly")
+	}
+
+	if cpArg0.Scheme != BlobStoreUrlScheme && cpArg1.Scheme != BlobStoreUrlScheme {
+		return errors.New("Must provide at least one blob:/ path to upload to or download from")
+	}
+
+	if cpArg0.Scheme == BlobStoreUrlScheme {
+		if force == false {
+			if _, err := os.Stat(cpArg1.Path); err == nil {
+				return errors.New("Destination file already exists on local machine; use --force to overwrite")
+			}
+		}
+		return b.DownloadFile(cpArg0.Path, cpArg1.Path)
+	} else {
+		if force == false {
+			fileStat, err := b.StatFile(cpArg1.Path)
+			if err != nil {
+				return err
+			}
+			if fileStat.Exists {
+				return errors.New("Destination file already exists on blobstore; use --force to overwrite")
+			}
+		}
+		return b.UploadFile(cpArg1.Path, cpArg0.Path, "")
+	}
+}
+
 
 func (b *BlobStoreClient) UploadFile(path string, source string, contentType string) error {
 	file, err := os.Open(source)
@@ -86,8 +131,12 @@ func (b *BlobStoreClient) DownloadFile(path, dest string) error {
 	return err
 }
 
-func (b *BlobStoreClient) CatFile(path string) error {
-	str, err := b.GetFileContents(path)
+func (b *BlobStoreClient) Cat(src *url.URL) error {
+	if src.Scheme != BlobStoreUrlScheme {
+		return errors.New("Must download files from blob:/")
+	}
+
+	str, err := b.GetFileContents(src.Path)
 	if err != nil {
 		return err
 	}
@@ -133,4 +182,23 @@ func (b *BlobStoreClient) ListPrefix(prefix string, recursive bool) ([]string, e
 
 func (b *BlobStoreClient) DeleteFile(path string) error {
 	return b.apiClient.DeleteFile(path)
+}
+
+func (b *BlobStoreClient) Exists(url_ url.URL) (bool, error) {
+	if url_.Scheme == "blob" {
+		f, err := b.StatFile(url_.Path)
+		if err != nil {
+			return false, err
+		}
+		return f.Exists, nil
+	} else {
+		if _, err := os.Stat(url_.Path); err == nil {
+			return true, nil
+		} else {
+			if os.IsNotExist(err) {
+				return false, nil
+			}
+			return false, err
+		}
+	}
 }
